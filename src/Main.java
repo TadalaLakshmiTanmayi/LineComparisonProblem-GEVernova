d17626cfa12479e564a5405879325ab6047aa60e
Tanmayi tadala <tanmayitadala@gmail.com>
2024-12-08 19:37:53 +0530

    UC4 added

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,102 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the coordinates for Line 1:");
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
+        Point p1 = new Point(sc.nextInt(), sc.nextInt());
+        Point p2 = new Point(sc.nextInt(), sc.nextInt());
+        Line line1 = new Line(p1, p2);
 
         System.out.println("Enter the coordinates for Line 2:");
-        int x3 = sc.nextInt();
-        int y3 = sc.nextInt();
-        int x4 = sc.nextInt();
-        int y4 = sc.nextInt();
-        CalculateLineLength findinglength=new CalculateLineLength();
-        LineEquality lineEquality = new LineEquality();
-        LineComparison lineComparison = new LineComparison();
-        int i =1;
-        do{
+        Point p3 = new Point(sc.nextInt(), sc.nextInt());
+        Point p4 = new Point(sc.nextInt(), sc.nextInt());
+        Line line2 = new Line(p3, p4);
+
+        int i = 1;
+        do {
             System.out.println("\nChoose an option:");
             System.out.println("1. Calculate the length of lines");
             System.out.println("2. Check equality of two lines");
             System.out.println("3. Compare two lines");
             System.out.println("Enter 0 to exit");
             int input = sc.nextInt();
-            switch (input){
+
+            switch (input) {
                 case 1:
-                    findinglength.lengthcalculation(x1,y1,x2,y2);
-                    findinglength.lengthcalculation(x3, y3, x4, y4);
+                    System.out.println("Length of Line 1: " + line1.calculateLength());
+                    System.out.println("Length of Line 2: " + line2.calculateLength());
                     break;
                 case 2:
-                    lineEquality.checkEquality(x1, y1, x2, y2, x3, y3, x4, y4);
+                    if (line1.equals(line2)) {
+                        System.out.println("Both lines are equal.");
+                    } else {
+                        System.out.println("The lines are not equal.");
+                    }
                     break;
                 case 3:
-                    lineComparison.compareLines(x1, y1, x2, y2, x3, y3, x4, y4);
+                    int comparisonResult = line1.compareTo(line2);
+                    if (comparisonResult > 0) {
+                        System.out.println("Line 1 is greater than Line 2.");
+                    } else if (comparisonResult < 0) {
+                        System.out.println("Line 1 is less than Line 2.");
+                    } else {
+                        System.out.println("Both lines are equal.");
+                    }
                     break;
                 default:
                     System.out.println("Wrong input");
                     break;
             }
+
             System.out.println("Enter 0 to exit");
             i = sc.nextInt();
-        }while (i!=0);
+        } while (i != 0);
         System.out.println("Thank You");
         sc.close();
+    }
+}
+
+// Point class to represent coordinates of a point
+class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+// Line class that represents a line segment with two points
+class Line implements Comparable<Line> {
+    Point start, end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    // Calculate the length of the line
+    public double calculateLength() {
+        return Math.sqrt(Math.pow(end.x - start.x, 2) + Math.pow(end.y - start.y, 2));
+    }
+
+    // Implement compareTo to compare lines based on their lengths
+    @Override
+    public int compareTo(Line other) {
+        double thisLength = this.calculateLength();
+        double otherLength = other.calculateLength();
+        return Double.compare(thisLength, otherLength); // Compare lengths
+    }
+
+    // Override equals to check if two lines are equal (based on length)
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Line other = (Line) obj;
+        return Double.compare(this.calculateLength(), other.calculateLength()) == 0;
     }
 }
